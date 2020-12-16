1268d368da3dbbf3afa8d4ac1df131d0e3ddb628
MertGonul91 <mertgonul91@gmail.com>
2020-12-16 17:41:04 +0100

    Shipping Screen

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+
+const ShippingScreen = ({ history }) => {
+  const [address, setAdress] = useState('');
+  const [city, setCity] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [country, setCountry] = useState('');
+  return (
+    <FormContainer>
+      <h1>Shipping</h1>
+      <Form>
+        <Form.Group controlId='name'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type='name'
+            placeholder='Enter name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default ShippingScreen;
