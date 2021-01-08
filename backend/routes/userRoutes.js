441afc4671f7fbac035d52ef4c172e835efe6f69
MertGonul91 <mertgonul91@gmail.com>
2021-01-08 15:09:04 +0100

    Admin Middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 import {
   authUser,
   getUserProfile,
+  getUsers,
   registerUser,
   updateUserProfile,
 } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router
   .route('/profile')
   .get(protect, getUserProfile)
